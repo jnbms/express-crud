2d597e5343e9d0bbfef8f0492422c78f2f69a195
jnbi <creat.bliv@gmail.com>
2021-09-03 00:20:57 +0900

    cm

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,4 +1,5 @@
 // import expres from 'express'
+import Userdb from '../model/model.js'
 import userdb from '../model/model.js'
 
 const create = (req,res) => {
@@ -22,13 +23,51 @@ const create = (req,res) => {
     })
 }
 const find = (req,res) => {
-
+    userdb.find()
+    .then(user=>{
+        res.send(user)
+    })
+    .catch(err=>{
+        res.status(500).send({message: err.message || "Erro Occurred while retiving user information"})
+    })
 }
 const update = (req,res) => {
+    if(!req.body){
+        return res
+            .status(400)
+            .send({message: "data to update can do not be empty"})
+    }
+    const id = req.params.id
+    Userdb.findByIdAndUpdate(id, req.body,{ useFineAndModify: false})
+        .then(data=>{
+            if(!data){
+                res.status(404).send({message: `Cannot Update user with ${id}. Maybe user not found!`})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err=>{
+            res.status(500).send({message: "Error Update user information"})
 
+        })
 }
 const remove = (req,res) => {
-
+    const id = req.params.id
+    Userdb.findByIdAndDelete(id)
+        .then(data=>{
+            if(!data){
+                res.status(404).send({message: `cannot Delete with id ${id}. Maybe id is wrong`})
+            }else{
+                res.send({
+                    message:"User was deleted successfully"
+                })
+            }
+        })
+        .catch(err=>{
+            res.satus(500).send({
+                message: 'could not delete User with id = ' + id 
+            })
+        })
 }
 
 
